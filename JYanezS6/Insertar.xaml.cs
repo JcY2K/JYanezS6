d20478327fdc65261a5b55be7ece1d6b828adac8
master <master@SMLAB108PC04_23>
2023-07-04 21:58:12 -0500

    Agregar archivos de proyecto.

---
+++ JYanezS6/Insertar.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JYanezS6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Insertar : ContentPage
+    {
+        private string URL = "http://192.168.10.34:8080/ws_uisrael/post.php";
+        public Insertar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try {
+                WebClient cliente = new WebClient();
+                var datos = new System.Collections.Specialized.NameValueCollection();
+                datos.Add("codigo", txtCodigo.Text);
+                datos.Add("nombre", txtNomrbre.Text);
+                datos.Add("apellido", txtApellido.Text);
+                datos.Add("edad", txtEdad.Text);
+
+                cliente.UploadValues(URL, "POST", datos);
+                Navigation.PushAsync(new MainPage());
+            } catch (Exception ex) 
+            {
+                DisplayAlert("ALERTA", ex.Message, "Cerrar");
+            
+            }
+            
+        }
+    }
+}
